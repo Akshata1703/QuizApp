a9effb928cf80247e91bc08349b17febeb396750
Akshata1703 <akshatatrivedi1@gmail.com>
2024-01-04 18:21:15 +0530

    first commit

---
+++ js/question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: " What is DOM in HTML?",
+        answer: " Convention for representing and interacting with objects in html documents",
+        options: [
+            "Language dependent application programming",
+            "Hierarchy of objects in ASP.NET",
+            "Application programming interface",
+            " Convention for representing and interacting with objects in html documents"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which of the following is not javascript data types?",
+        answer: "All of the mentioned",
+        options: [
+            "Null type",
+            "Undefined type",
+            "Number type",
+            "All of the mentioned"
+        ]
+    },
+    {
+        numb: 3,
+        question: " HTML is a subset of ___________",
+        answer: "SGML",
+        options: [
+            "SGMT",
+            "SGML",
+            "SGME",
+            "XHTML"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which of the following can be used to call a JavaScript Code Snippet?",
+        answer: "Function/Method",
+        options: [
+            "Function/Method",
+            "Preprocessor",
+            "Triggering Event",
+            "RMI"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of the following scoping type does JavaScript use?",
+        answer: "Lexical",
+        options: [
+            "Sequential",
+            "Segmental",
+            "Lexical",
+            "Literal"
+        ]
+    },
+]
